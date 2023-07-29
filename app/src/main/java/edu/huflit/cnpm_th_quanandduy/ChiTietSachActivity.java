e1b82ad68a97633c07f64a93f813ed55cf40b1d2
NguyenDuy <97612815+racingboy2@users.noreply.github.com>
2023-07-29 20:47:47 +0700

    init

---
+++ app/src/main/java/edu/huflit/cnpm_th_quanandduy/ChiTietSachActivity.java
@@ -0,0 +1,14 @@
+package edu.huflit.cnpm_th_quanandduy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChiTietSachActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chi_tiet_sach);
+    }
+}
